36413282b66aee5b1d971c56a50ea2b69e23ff1d
Monty Dimkpa <cmdimkpa@gmail.com>
2021-05-28 05:53:28 +0100

    version updated to 1.8

--- migrate.py
+++ migrate.py
@@ -1,6 +1,7 @@
 # Project Description: Migrate data from Google Sheets to Snowflake
 # Author: Monty Dimkpa
-# Version: 1.0
+# Version: 1.8
+# Last Updated: May 2021
 # Inquiries: cmdimkpa@gmail.com
 
 # import required libraries
@@ -10,8 +11,7 @@ import sys
 import snowflake.connector
 import gspread
 from time import sleep
-
-RATE_LIMIT_DELAY_SECS = 3
+import argparse
 
 # determine script location and adjust for Operating System
 THIS_DIR = os.getcwd()  
@@ -28,56 +28,47 @@ snowflake_creds_file = THIS_DIR+"snowflake.json"
 googlesheets_creds_file = THIS_DIR+"googlesheets.json"
 
 # extract required runtime arguments from CLI
-args = sys.argv
+parser = argparse.ArgumentParser()
+
+def parse_csv(text, description):
+    try:
+        return [x for x in text.split(",") if x]
+    except:
+        print('Error: please provide [%s] as a comma-separated sequence of values e.g. 1,2,3,4' % description)
+        sys.exit()
+
+parser.add_argument('--rate-limit-delay', type=int, default=2.7, help='Seconds to wait before reading next row from Google Sheets API')
+parser.add_argument('--max-concurrent-write', type=int, default=10, help='Number of rows to write to Snowflake at once')
+parser.add_argument('--target-sheet-name', type=str, required=True, help='GOOGLESHEETS_TARGET_SHEET')
+parser.add_argument('--columns-to-read', type=str, required=True, help='GOOGLESHEETS_READ_COLS (comma separated)')
+parser.add_argument('--max-rows-to-copy', type=int, required=True, help='GOOGLESHEETS_MAX_ROW')
+parser.add_argument('--target-warehouse', type=str, required=True, help='SNOWFLAKE_TARGET_WAREHOUSE')
+parser.add_argument('--target-database', type=str, required=True, help='SNOWFLAKE_TARGET_DATABASE')
+parser.add_argument('--target-table', type=str, required=True, help='SNOWFLAKE_TARGET_TABLE')
+parser.add_argument('--target-schema', type=str, required=True, help='SNOWFLAKE_TARGET_SCHEMA')
+parser.add_argument('--target-field-names', type=str, required=True, help='SNOWFLAKE_TARGET_FIELD_NAMES (comma separated)')
+parser.add_argument('--target-role', type=str, required=True, help='SNOWFLAKE_TARGET_ROLE')
+
+args = parser.parse_args()
+
+# process arguments
+RATE_LIMIT_DELAY_SECS = args.rate_limit_delay
+CONCURRENT_WRITE_LIMIT_MAX = args.max_concurrent_write
 
 # Google Sheets arguments
-try:
-    googlesheets_target_sheet = args[1]
-except:
-    print("Error: please provide GOOGLESHEETS_TARGET_SHEET as 1st argument")
-    sys.exit()
-try:
-    googlesheets_read_cols = [x for x in args[2].split(",") if x]
-except:
-    print("Error: please provide GOOGLESHEETS_READ_COLS as 2nd argument")
-    sys.exit()
-try:
-    googlesheets_max_row = int(float(args[3]))
-except:
-    print("Error: please provide GOOGLESHEETS_MAX_ROW as 3rd argument")
-    sys.exit()
+googlesheets_target_sheet = args.target_sheet_name
+googlesheets_read_cols = parse_csv(args.columns_to_read, 'GOOGLESHEETS_READ_COLS')
+googlesheets_max_row = args.max_rows_to_copy
 
 # Snowflake arguments
-try:
-    snowflake_target_warehouse = args[4]
-except:
-    print("Error: please provide SNOWFLAKE_TARGET_WAREHOUSE as 4th argument")
-    sys.exit()
-try:
-    snowflake_target_database = args[5]
-except:
-    print("Error: please provide SNOWFLAKE_TARGET_DATABASE as 5th argument")
-    sys.exit()
-try:
-    snowflake_target_table = args[6]
-except:
-    print("Error: please provide SNOWFLAKE_TARGET_TABLE as 6th argument")
-    sys.exit()
-try:
-    snowflake_target_schema = args[7]
-except:
-    print("Error: please provide SNOWFLAKE_TARGET_SCHEMA as 7th argument")
-    sys.exit()
-try:
-    snowflake_target_field_names = [x for x in args[8].split(",") if x]
-except:
-    print("Error: please provide SNOWFLAKE_TARGET_FIELD_NAMES as 8th argument")
-    sys.exit()
-try:
-    snowflake_target_role = args[9]
-except:
-    print("Error: please provide SNOWFLAKE_TARGET_ROLE as 9th argument")
-    sys.exit()
+snowflake_target_warehouse = args.target_warehouse
+snowflake_target_database = args.target_database
+snowflake_target_table = args.target_table
+snowflake_target_schema = args.target_schema
+snowflake_target_field_names = parse_csv(args.target_field_names, 'SNOWFLAKE_TARGET_FIELD_NAMES')
+snowflake_target_role = args.target_role
+
+# utility functions
 
 def read_json_file(file):
     process = open(file,"rb+")
@@ -91,6 +82,14 @@ def apply_type(x):
         return x
     except:
         return "'%s'" % x
+
+def convert(t):
+    new_t = []
+    for entry in t:
+        comps = entry.split(",")
+        if comps != ["'None'"]*len(comps):
+            new_t.append("(%s)" % entry)
+    return len(new_t), ",".join(new_t)
 
 # pull your Snowflake credentials; create connection and cursor object
 credsSF = read_json_file(snowflake_creds_file)
@@ -132,7 +131,6 @@ class EventManager:
         self.readLimit = None
         self.targetCursor = None
         self.targetTable = None
-        self.SQLInsertTemplate = 'INSERT INTO %s (%s) VALUES (%s)'
         self.eventlogProcess = None
 
     def update_target_table(self, table):
@@ -165,28 +163,44 @@ class EventManager:
     def run(self):
         if self.targetTable and self.sheetObject and self.readLimit and self.targetCursor and self.realtimeLog["columnList"] and self.realtimeLog["fieldNames"]:
             writes = 0
-            EOF = False
-            while self.readLimit > self.realtimeLog["onRow"] and not EOF:
-                self.realtimeLog["onRow"]+=1
+            query_tuple = ("%s.%s.%s" % (snowflake_target_database, snowflake_target_schema, self.targetTable), ",".join(self.realtimeLog["fieldNames"]),)
+            values_tuple = ()
+            while self.readLimit > self.realtimeLog["onRow"]:
+                remaining_rows = self.readLimit - self.realtimeLog["onRow"]
+                CONCURRENT_WRITE_LIMIT = CONCURRENT_WRITE_LIMIT_MAX if remaining_rows >= CONCURRENT_WRITE_LIMIT_MAX else remaining_rows
+                self.SQLInsertTemplate = 'INSERT INTO %s (%s) VALUES ' + ','.join(['(%s)' for i in range(CONCURRENT_WRITE_LIMIT)])
+                self.realtimeLog["onRow"] += 1
                 values = ",".join([apply_type(self.sheetObject.acell("%s%s" % (
                     column, self.realtimeLog["onRow"])).value) for column in self.realtimeLog["columnList"]])
-                sql = self.SQLInsertTemplate % ("%s.%s.%s" % (snowflake_target_database, snowflake_target_schema, self.targetTable), ",".join(self.realtimeLog["fieldNames"]), values)
-                if 'None' in sql:
-                    EOF = True
-                else:
-                    self.targetCursor.execute(sql)
-                    self.realtimeLog["sql"].append(sql)
-                    self.log()
-                    writes+=1
-                    print("200 OK on Write #%s - Query: %s" % (writes, sql))
-                    sleep(RATE_LIMIT_DELAY_SECS)
+                values_tuple += (values,)
+                if self.realtimeLog["onRow"] % CONCURRENT_WRITE_LIMIT == 0 or remaining_rows <= CONCURRENT_WRITE_LIMIT:
+                    try:
+                        use_tuple = query_tuple + values_tuple
+                        sql = self.SQLInsertTemplate % use_tuple
+                    except:
+                        CONCURRENT_WRITE_LIMIT, values_tuple = convert(values_tuple)
+                        sql = 'INSERT INTO %s (%s) VALUES '+values_tuple
+                        sql = sql % ("%s.%s.%s" % (snowflake_target_database, snowflake_target_schema, self.targetTable), ",".join(self.realtimeLog["fieldNames"]))
+                    try:
+                        self.targetCursor.execute(sql)
+                        self.realtimeLog["sql"].append(sql)
+                        self.log()
+                        writes+=CONCURRENT_WRITE_LIMIT
+                        values_tuple = ()
+                        print("200 OK on Write #%s - Query: %s" % (writes, sql))
+                    except Exception as err:
+                        err = str(err)
+                        if 'EOF' not in err:
+                            print("Error occured while writing: %s" % err)
+                        self.shutdown()
+                sleep(RATE_LIMIT_DELAY_SECS)
             self.shutdown()
         else:
             print("Error: please provide required command line arguments to begin")
             sys.exit()
 
     def shutdown(self):
-        self.log()
+        sys.exit()
 
 # create and update migration event manager
 currentEvents = EventManager()
